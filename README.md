5ed3a19eacd8bb77129ea295caa77964bce1e7b2
Srisugihartika <36659998+Srisugihartika@users.noreply.github.com>
2018-05-22 15:45:41 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Project_Mahasiswa
+# uts-web
---
+++ koneksi.php
@@ -0,0 +1,15 @@
+<?php
+class Koneksi{
+  private $server="localhost";
+  private $username="root";
+  private $password = "";
+  private $db = "data_mahasiswa";
+  private $hubungan;
+
+  function ambilKoneksi(){
+    $hubungan= new mysqli($this ->server, $this->username,
+    $this->password, $this->db);
+    return $hubungan;
+  }
+}
+ ?>
